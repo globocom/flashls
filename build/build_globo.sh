40b48b17e3c3d4062c02a5995c357ba359a0ef39
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2014-07-10 15:30:49 -0300

    build_globo: enhance parameters, using air sdk 14

--- build/build_globo.sh
+++ build/build_globo.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
-FLEXPATH=~/flex/
+FLEXPATH=~/airsdk/
 
 OPT_DEBUG="-library-path+=../lib/as3crypto.swc \
     -use-network=false \
     -optimize=true \
-    -incremental=true \
-    -static-link-runtime-shared-libraries=true \
+    -compress=true \
+    -strict=true \
+    -use-gpu=true \
     -define=CONFIG::LOGGING,true"
 
 OPT_RELEASE="-library-path+=../lib/as3crypto.swc \
     -use-network=false \
     -optimize=true \
-    -incremental=true \
-    -static-link-runtime-shared-libraries=true \
     -define=CONFIG::LOGGING,false"
 
 echo "Compiling bin/debug/flashls.swc"
@@ -29,7 +28,6 @@ $FLEXPATH/bin/compc \
     -output ../bin/release/flashls.swc \
     -target-player="10.1"
 
-
 echo "Compiling bin/release/HLSPlayer.swf"
 $FLEXPATH/bin/mxmlc ../src/com/globo/Player.as \
     -source-path ../src \
@@ -38,8 +36,9 @@ $FLEXPATH/bin/mxmlc ../src/com/globo/Player.as \
     -library-path+=../lib/blooddy_crypto.swc \
     -target-player="11.1" \
     -default-size 480 270 \
-    -default-background-color=0x000000
-./add-opt-in.py ../bin/release/flashlsChromeless.swf
+    -default-background-color=0x000000 \
+    -default-frame-rate=60
+./add-opt-in.py ../bin/release/HLSPlayer.swf
 
 echo "Compiling bin/debug/HLSPlayer.swf"
 $FLEXPATH/bin/mxmlc ../src/com/globo/Player.as \
@@ -49,6 +48,7 @@ $FLEXPATH/bin/mxmlc ../src/com/globo/Player.as \
     -library-path+=../lib/blooddy_crypto.swc \
     -target-player="11.1" \
     -default-size 480 270 \
-    -default-background-color=0x000000
-./add-opt-in.py ../bin/debug/flashlsChromeless.swf
+    -default-background-color=0x000000 \
+    -default-frame-rate=60
+./add-opt-in.py ../bin/debug/HLSPlayer.swf
 
